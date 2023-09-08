5e2f6011daa7d1e534e1cffc59d2dde5a746fe2e
Bullko <krajnak.anton@gmail.com>
2023-09-08 14:51:25 +0200

    css for responsive

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,7 +2,7 @@ import "./footer.css";
 
 export default function Footer() {
   return (
-    <div>
+    <div className="footer">
       <p className="info">
         Made by @antonK visit my{" "}
         <a href="https://portfolio-antonk.netlify.app/" target="blank">
